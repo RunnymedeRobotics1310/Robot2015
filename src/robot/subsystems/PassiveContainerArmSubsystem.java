d931da7d8b841cefbb00ffd5adc6d4754584471f
Brock Watling <brock_watling7@hotmail.com>
2015-03-27 18:27:39 -0400

    Fixed the elevator stuff that gregor wanted

--- src/robot/subsystems/PassiveContainerArmSubsystem.java
+++ src/robot/subsystems/PassiveContainerArmSubsystem.java
@@ -24,44 +24,44 @@ public class PassiveContainerArmSubsystem extends RunnymedeSubsystem {
 	int numTotes = 0;
 	
 	public void update(boolean actuateArm, boolean pickupContainer, boolean deployHolder) {
-		
-		ToteElevatorLevel level = Robot.toteElevatorSubsystem.getLevel();
-		
-		if(DriverStation.getInstance().isAutonomous()) {
-			holderSolenoid.set(DoubleSolenoid.Value.kForward);
-		} else if ((level == ToteElevatorLevel.FIVE && deployHolder && !Robot.toteElevatorSubsystem.isMoving()) || (deployHolder && !Robot.toteElevatorSubsystem.getUpperLimit())) {
-			holderSolenoid.set(DoubleSolenoid.Value.kForward);
-		} else if(!deployHolder) {// && ((level == ToteElevatorLevel.FIVE || level == ToteElevatorLevel.FOUR) && !Robot.toteElevatorSubsystem.isMoving())) {
-			holderSolenoid.set(DoubleSolenoid.Value.kReverse);
-		}
-		
-		if(pickupContainer) {
-			Robot.oi.overrideContainerArmToggle(true);
-			numTotes = 0;
-		}
-		
-		lightSensorLatch.update(!lightSensor.get());
-		
-		if(numTotes < 2 && lightSensorLatch.getState()) {// &&
-//				!(Robot.toteElevatorSubsystem.getLevel() == ToteElevatorLevel.HALF &&
-//				Robot.toteElevatorSubsystem.getUpperLimit())) {
-			numTotes++;
-			armSolenoid.set(false);
-			pulseTimer.disable();
-			pulseTimer.start();
-			lightSensorLatch.setState(false);
-		}
-		
-		SmartDashboard.putNumber("number of totes", numTotes);
-		SmartDashboard.putBoolean("puse timer isExpired", pulseTimer.isExpired());
-		SmartDashboard.putBoolean("light sensor latch", lightSensorLatch.getState());
-		
-		if(pulseTimer.isExpired() && Robot.toteElevatorSubsystem.getLevel() != ToteElevatorLevel.FLOOR) {
-			armSolenoid.set(Robot.toteIntakeSubsystem.getPassiveContainerSaftey() || (!pickupContainer && actuateArm));
-		} else if(pulseTimer.isExpired() && lightSensor.get()) {
-			armSolenoid.set(false);
-		}
+		if(!DriverStation.getInstance().isAutonomous()) {
+			ToteElevatorLevel level = Robot.toteElevatorSubsystem.getLevel();
 
+			if(DriverStation.getInstance().isAutonomous()) {
+				holderSolenoid.set(DoubleSolenoid.Value.kForward);
+			} else if ((level == ToteElevatorLevel.FIVE && deployHolder && !Robot.toteElevatorSubsystem.isMoving()) || (deployHolder && !Robot.toteElevatorSubsystem.getUpperLimit())) {
+				holderSolenoid.set(DoubleSolenoid.Value.kForward);
+			} else if(!deployHolder) {// && ((level == ToteElevatorLevel.FIVE || level == ToteElevatorLevel.FOUR) && !Robot.toteElevatorSubsystem.isMoving())) {
+				holderSolenoid.set(DoubleSolenoid.Value.kReverse);
+			}
+
+			if(pickupContainer) {
+				Robot.oi.overrideContainerArmToggle(true);
+				numTotes = 0;
+			}
+
+			lightSensorLatch.update(!lightSensor.get());
+
+			if(numTotes < 2 && lightSensorLatch.getState()) {// &&
+				//				!(Robot.toteElevatorSubsystem.getLevel() == ToteElevatorLevel.HALF &&
+				//				Robot.toteElevatorSubsystem.getUpperLimit())) {
+				numTotes++;
+				armSolenoid.set(false);
+				pulseTimer.disable();
+				pulseTimer.start();
+				lightSensorLatch.setState(false);
+			}
+
+			SmartDashboard.putNumber("number of totes", numTotes);
+			SmartDashboard.putBoolean("puse timer isExpired", pulseTimer.isExpired());
+			SmartDashboard.putBoolean("light sensor latch", lightSensorLatch.getState());
+
+			if(pulseTimer.isExpired() && (Robot.toteElevatorSubsystem.getLevel() != ToteElevatorLevel.FLOOR)) {
+				armSolenoid.set(Robot.toteIntakeSubsystem.getPassiveContainerSaftey() || (!pickupContainer && actuateArm));
+			} else if(pulseTimer.isExpired() && lightSensor.get() && !Robot.toteElevatorSubsystem.isMoving()) {
+				armSolenoid.set(false);
+			}
+		}
 	}
 
 	@Override
@@ -83,7 +83,7 @@ public class PassiveContainerArmSubsystem extends RunnymedeSubsystem {
 
 	@Override
 	protected void initDefaultCommand() {
-		setDefaultCommand(null);//new DefaultPassiveContainerCommand());
+		setDefaultCommand(new DefaultPassiveContainerCommand());
 	}
 
 }
